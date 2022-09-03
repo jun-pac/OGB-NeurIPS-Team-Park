ec7bf5dce5f009e71b98160385eeb1ad702350ec
Sejun Park <oiocha@pitzer-rw02.ten.osc.edu>
2022-09-03 10:23:39 -0400

    Add Deepmind code

--- examples/lsc/mag240m/rgnn.py
+++ examples/lsc/mag240m/rgnn.py
@@ -2,8 +2,12 @@ import argparse
 import glob
 import os
 import os.path as osp
+import sys
 import time
 from typing import List, NamedTuple, Optional
+
+
+sys.path.insert(0,'/users/PAS1289/oiocha/OGB-NeurIPS-Team-Park')
 
 import numpy as np
 import torch
@@ -12,7 +16,8 @@ from ogb.lsc import MAG240MDataset, MAG240MEvaluator
 from pytorch_lightning import (LightningDataModule, LightningModule, Trainer,
                                seed_everything)
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.metrics import Accuracy
+#from pytorch_lightning.metrics import Accuracy
+from torchmetrics.functional import accuracy as Accuracy
 from torch import Tensor
 from torch.nn import BatchNorm1d, Dropout, Linear, ModuleList, ReLU, Sequential
 from torch.optim.lr_scheduler import StepLR
